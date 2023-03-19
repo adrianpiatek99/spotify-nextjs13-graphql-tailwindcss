6abeec7a86a4fe3315deff47a812bff045e0fb84
Adrian Piątek <adr654231@gmail.com>
2023-03-19 22:30:55 +0100

    Add Input component

---
+++ src/components/core/Input/Input.tsx
@@ -0,0 +1,89 @@
+import type { ComponentPropsWithRef, FC, Ref } from "react";
+import React, { forwardRef, useState } from "react";
+
+import { EyeIcon, EyeInvisibleIcon, WarningIcon } from "@/icons";
+import { twMerge } from "tailwind-merge";
+
+import type { InputType } from "./Input.type";
+
+interface InputProps extends Omit<ComponentPropsWithRef<"input">, "type" | "size"> {
+  name: string;
+  label: string;
+  onValueChange?: (value: string) => void;
+  disabled?: boolean;
+  type?: InputType;
+  error?: string;
+}
+
+const inputClasses = {
+  hover: "enabled:hover:shadow-[inset_0px_0px_0px_1px_theme('colors.gray.200')]",
+  focus: "enabled:focus-visible:shadow-[inset_0px_0px_0px_2px_theme('colors.gray.200')]",
+  error:
+    "shadow-[inset_0px_0px_0px_1px_theme('colors.error')] enabled:hover:shadow-[inset_0px_0px_0px_1px_theme('colors.error')] enabled:focus-visible:shadow-[inset_0px_0px_0px_2px_theme('colors.error')]",
+  disabled: "disabled:opacity-50",
+  placeholder: "placeholder:text-gray-200"
+};
+
+export const Input: FC<InputProps> = forwardRef(
+  (
+    {
+      name,
+      label,
+      value,
+      error,
+      type = "text",
+      onValueChange = () => null,
+      maxLength = 255,
+      className = "",
+      ...props
+    },
+    ref: Ref<HTMLInputElement>
+  ) => {
+    const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+    const isPasswordType = type === "password";
+    const inputType = isPasswordVisible ? "text" : type;
+
+    return (
+      <div className="flex flex-col gap-2 min-w-[50px]">
+        <label className="font-semibold" htmlFor={label}>
+          {label}
+        </label>
+        <div className="relative flex w-full">
+          <input
+            className={twMerge(
+              `bg-background2 w-full px-[14px] py-[15.5px] placeholder:text-gray-200 outline-none rounded shadow-[inset_0px_0px_0px_1px_theme('colors.gray.DEFAULT')] duration-200 ${
+                inputClasses.disabled
+              } ${inputClasses.hover} ${inputClasses.focus} ${inputClasses.placeholder} ${
+                error && inputClasses.error
+              } ${className}`
+            )}
+            id={label}
+            type={inputType}
+            name={name}
+            aria-label={label}
+            value={value}
+            onChange={e => onValueChange(e.target.value)}
+            autoComplete="off"
+            maxLength={maxLength}
+            placeholder={label}
+            {...props}
+            ref={ref}
+          />
+          {isPasswordType && (
+            <div className="absolute flex gap-[4px] right-[20px] top-[50%] -translate-y-2/4">
+              <div onClick={() => setIsPasswordVisible(prev => !prev)}>
+                {isPasswordVisible ? <EyeInvisibleIcon /> : <EyeIcon />}
+              </div>
+            </div>
+          )}
+        </div>
+        {error && (
+          <div className="flex items-center gap-1 px-[1px] text-error">
+            <WarningIcon className="w-[16px] h-[16px]" />
+            <span>{error}</span>
+          </div>
+        )}
+      </div>
+    );
+  }
+);
