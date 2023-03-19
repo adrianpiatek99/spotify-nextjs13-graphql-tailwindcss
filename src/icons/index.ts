b9f342a5daedee05c9d3c324bf317f7d7ebb9074
Adrian Piątek <adr654231@gmail.com>
2023-03-19 16:13:09 +0100

    feat: Configure svgr cli (#3)
    * Add @svgr/cli and rimraf packages
    
    * Add svgr-icons config
    
    * Add generate-icons script
    
    * Add icons
    
    * format

---
+++ src/icons/index.ts
@@ -0,0 +1,6 @@
+export { default as AppleIcon } from "./AppleIcon";
+export { default as ChevronIcon } from "./ChevronIcon";
+export { default as FacebookIcon } from "./FacebookIcon";
+export { default as GoogleIcon } from "./GoogleIcon";
+export { default as SpotifyIcon } from "./SpotifyIcon";
+export { default as WarningIcon } from "./WarningIcon";
