e64204f98ead9e1868849137c8338452cd60e855
Adrian Piątek <adr654231@gmail.com>
2023-03-20 18:15:14 +0100

    Remove unnecessary className

--- src/components/core/Loader/Loader.tsx
+++ src/components/core/Loader/Loader.tsx
@@ -11,7 +11,7 @@ export const Loader: FC<LoaderProps> = ({ className = "" }) => {
   return (
     <span
       className={twMerge(
-        `spinner after:content-[''] after:absolute after:top-[calc(50%-13px)] after:left-[calc(50%-13px)] after:w-[26px] after:h-[26px] after:rounded-[50%] after:border-t-[3px] after:border-l-[3px] after:border-r-[3px] after:border-t-foreground after:border-l-foreground after:border-r-foreground/10 after:animate-animation-spinner ${className}`
+        `spinner after:content-[''] after:absolute after:top-[calc(50%-13px)] after:left-[calc(50%-13px)] after:w-[26px] after:h-[26px] after:rounded-[50%] after:border-t-[3px] after:border-l-[3px] after:border-r-[3px] after:border-t-foreground after:border-l-foreground after:border-r-foreground/5 after:animate-animation-spinner ${className}`
       )}
     />
   );
